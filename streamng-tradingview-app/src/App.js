19dafebbcd093219c3e5ed7d3208756c07268994
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2023-10-17 19:39:26 +0530

    project init

---
+++ streamng-tradingview-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import {StreamingData} from './bitquery'
+function App() {
+  return (
+    <div className="App">
+      <StreamingData/>
+    </div>
+  );
+}
+
+export default App;
